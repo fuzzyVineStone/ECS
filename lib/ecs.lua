c941e3785841c4de103e6f781317317496145e1b
Valdemar Lorenzen <fuzzyvinestone@gmail.com>
2019-03-11 11:20:09 +0100

    initial upload

---
+++ lib/ecs.lua
@@ -0,0 +1,11 @@
+entity	  = require("core.entity")
+component = require("core.component")
+system	  = require("core.system")
+world	  = require("core.world")
+
+return {
+	entity	  = entity,
+	component = component,
+	system	  = system,
+	world	  = world
+}
